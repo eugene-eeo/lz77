3c6dbfa431baa46526f8668d1199c812d0a26bc2
Eeo Jun <141bytes@gmail.com>
2019-01-22 17:58:44 +0000

    add corpus and results gathering script

---
+++ encoder_runtime.py
@@ -0,0 +1,29 @@
+import json
+import time
+from lz77 import inflate, deflate
+
+
+for filename in [
+        "corpus/mini-beers.txt",
+        "corpus/mini-names.txt",
+        "corpus/beers.txt",
+        "corpus/names.txt",
+        ]:
+    buff = open(filename, "rb").read()
+    for L in [15, 31, 63, 127, 255, 511]:
+        for W in [255, 511, 1023, 2047, 4095, 65535]:
+            for i in range(100):
+                deflate_t0 = time.time()
+                x = deflate(buff, W, L)
+                deflate_t1 = time.time()
+                inflate_t0 = time.time()
+                inflate(x, W, L)
+                inflate_t1 = time.time()
+                print(json.dumps([
+                    filename,                  # filename
+                    L,                         # L
+                    W,                         # W
+                    len(x) / len(buff),        # compression ratio
+                    deflate_t1 - deflate_t0,   # deflate/encode time
+                    inflate_t1 - inflate_t0,   # inflate/decode time
+                ]))
---
+++ generate_beers.py
@@ -0,0 +1,20 @@
+with open('corpus/beers.txt', mode="w") as fp:
+    for i in range(99, 29, -1):
+        fp.write("{n} bottles of beer on the wall, {n} bottles of beer.\n".format(n=i))
+        if i == 1:
+            fp.write("Take one down and pass it around, no more bottles of beer on the wall.\n")
+            break
+        fp.write("Take one down and pass it around, {m} bottles of beer on the wall.\n".format(m=i-1))
+    #fp.write("No more bottles of beer on the wall, no more bottles of beer.\n")
+    #fp.write("Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+
+
+
+with open('corpus/mini-beers.txt', mode="w") as fp:
+    for i in range(99, 0, -1):
+        fp.write("{n} bottles of beer on the wall, {n} bottles of beer.\n".format(n=i))
+
+
+
+with open('corpus/mini-names.txt', mode="wb") as fp:
+    fp.write(open('corpus/names.txt', mode="rb").read()[:5130])
